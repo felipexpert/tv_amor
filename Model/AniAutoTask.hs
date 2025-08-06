70406c9a90c358a0c9fc5978ad815315d0bf5f0d
Felipe Carmona Miquilini <felipe.miquilini@gmail.com>
2025-08-06 06:29:52 -0300

    progresso

--- Model/AniAutoTask.hs
+++ Model/AniAutoTask.hs
@@ -1,5 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use newtype instead of data" #-}
+{-# HLINT ignore "Unused LANGUAGE pragma" #-}
 
 -- MyModule.hs
 module Model.AniAutoTask where
@@ -9,6 +12,36 @@ import Data.Text (Text)
 import GHC.Generics (Generic) 
 
 data AniAutoTask = AniAutoTask
-    { atTaskName :: Text -- temp
-    , atDescription :: Text -- temp
-    } deriving (Show, Eq, Generic)
+    { aatActions :: [TPeAction]
+    -- depois pode ter mais campos
+    } 
+    deriving (Show, Eq, Generic)
+
+data TPeAction = TPeAction
+    { tpaNumber :: TPeNumber
+    , tpaAction :: AAction
+    }
+    deriving (Show, Eq, Generic)
+
+data AAction
+    = ASpeech
+        { asAudioWav :: FilePath -- Caminho do arquivo de áudio
+        , asStartTime :: Int -- Tempo de início da fala
+        }
+    | AGesture
+        { agGesture :: AGGesture 
+        , agStartTime :: Int -- Tempo de início do gesto 
+        }
+    deriving (Show, Eq, Generic)
+
+-- você pode criar gestos compostos no CA4 e vincular aqui!
+data AGGesture
+    = AGWave1
+    | AGThink1
+    deriving (Show, Eq, Generic)
+
+-- para identificar o sprite da pessoa na TPeAction
+data TPeNumber 
+    = PNumber1
+    | PNumber2
+    deriving (Show, Eq, Generic)
---
+++ Model/AudioInfo.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use newtype instead of data" #-}
+{-# HLINT ignore "Unused LANGUAGE pragma" #-}
+
+-- MyModule.hs
+module Model.AudioInfo where
+
+-- Utiliza para comunicar com o GuidoLang, parâmetro de envio
+newtype AudiosRequest = [AudioRequest] deriving (Show, Eq, Generic)
+
+-- Utiliza para comunicar com o GuidoLang, resultado
+newtype AudiosInfo = [AudioInfo] deriving (Show, Eq, Generic)
+
+data AudioInfo = AudioInfo
+    -- Caminho do arquivo de áudio, pode ser um número 00001.wav, 00002.wav, etc.
+    { aiFilePath :: FilePath 
+    -- Duração do áudio em milissegundos
+    , aiDuration :: Int 
+    } deriving (Show, Eq, Generic)
+
+data AudioRequest = AudioRequest
+    { arText :: Text -- Texto a ser convertido em áudio
+    , arConfig :: AudioRequestConfig -- Configurações do áudio
+    } deriving (Show, Eq, Generic)
+
+data AudioRequestConfig = AudioRequestConfig
+    { arcVoice :: Text -- Voz a ser utilizada
+    -- vai ter mais campos para parâmetros do timbre da voz
+    } deriving (Show, Eq, Generic)
+
+-- Função para solicitar áudios ao GuidoLang
+requestAudiosIO :: AudiosRequest -> IO AudiosInfo
+requestAudiosIO = undefined
