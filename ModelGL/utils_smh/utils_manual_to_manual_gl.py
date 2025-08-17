65d8d65574808dd95528a53834c3561ca1962aff
Felipe Carmona Miquilini <felipe.miquilini@gmail.com>
2025-08-17 11:35:30 -0300

    progresso

--- ModelGL/utils_smh/utils_manual_to_manual_gl.py
+++ ModelGL/utils_smh/utils_manual_to_manual_gl.py
@@ -24,9 +24,25 @@ def subtract_works_done(m: ManualForGUI, ms: ManualGL):
 def is_work_already_done(w:Work, ms:ManualGL) -> bool:
     pass
 
+def ensure_official_chrome_profile(cprofs: List[ChromeProfile]) -> ChromeProfile:
+    cprof_official:ChromeProfile = None
+    for cprof in cprofs:
+        cprof:ChromeProfile = cprof
+        if cprof.storeId is None:
+            cprof_official = cprof
+            break
+    if cprof_official is None:
+        # inicializa sem trabalhos
+        cprof_official:ChromeProfile = ChromeProfile(storeId=None,chromeProfile="Default",socialNetworks=[])
+        # adiciona à lista que veio
+        cprofs.append(cprof_official)
+    return cprof_official        
+        
+
 def manual_to_manual_for_gui(m: Manual, config:ConfigSmh) -> ManualForGUI:
     debug = False
-    chrome_profiles: list[ChromeProfile] = []
+    chrome_profiles: List[ChromeProfile] = []
+    snworks_official_from_others:List[List[SocialNetworkWorks]] = []
     for acc in m.mAccounts:
         store_opt:Optional[Store] = acc.maAutoShareStoreOpt
         # if not store_opt: continue # por enquanto pula oficial, porque nao tem
@@ -37,14 +53,35 @@ def manual_to_manual_for_gui(m: Manual, config:ConfigSmh) -> ManualForGUI:
         # if profile_maybe.is_some():
         if profile_maybe:
             p:Profile = profile_maybe.unwrap()
-            list_snworks:List[SocialNetworkWorks] = list_of_social_network_works(acc)
+            (list_snworks,list_snworks_official) = list_of_social_network_works(acc)
             if debug: print("list_snworks", list_snworks)
             chrome_profile:ChromeProfile = ChromeProfile(storeId=store_id_opt, chromeProfile=p.chromeProfile, socialNetworks=list_snworks)
             chrome_profiles.append(chrome_profile)
+            snworks_official_from_others.append(list_snworks_official)
         else:
             print_alt("ATENÇÃO: Não encontramos um profile para store_id_opt={store_id_opt}, em config_smh.json")
+    chrome_profile_official = ensure_official_chrome_profile(chrome_profiles)
+    add_snworks_to_chrome_profile(chrome_profile_official, snworks_official_from_others)
     manual_gl:ManualForGUI = ManualForGUI(chromeProfiles=chrome_profiles)
     return manual_gl
+
+def add_snworks_to_chrome_profile(cprof: ChromeProfile, snworks_list_list: List[List[SocialNetworkWorks]]):
+    # adiciona ou cria caso nao tenha
+    for snworks_list in snworks_list_list:
+        for snworks in snworks_list:
+            snworks2_opt = get_social_network_works_opt(cprof, snworks.socialNetwork)
+            if snworks2_opt:
+                # insere no começo, sem criar outra lista
+                snworks2_opt.works[:0] = snworks.works
+            else:
+                cprof.socialNetworks.append(snworks)
+        
+
+def get_social_network_works_opt(cprof: ChromeProfile, sn:SocialNetwork) -> Optional[SocialNetworkWorks]:
+    for snworks in cprof.socialNetworks:
+        if snworks.socialNetwork == sn:
+            return snworks
+    return None
         
 # Continua daqui - estou adicionando os oficiais que vem junto com os de cliente, para
 # depois concatenar com as atividades do profile do chrome para oficiais
@@ -52,26 +89,33 @@ def manual_to_manual_for_gui(m: Manual, config:ConfigSmh) -> ManualForGUI:
 def list_of_social_network_works(acc: ManualAccount) -> Tuple[List[SocialNetworkWorks], List[SocialNetworkWorks]]:
     ig_works_works:List[Work] = []
     tk_works_works:List[Work] = []
+    ig_works_works_official:List[Work] = []
+    tk_works_works_official:List[Work] = []
     for manual_work in acc.maWorks:
         if manual_work.mwActionIgOpt:
             (w, w_official_opt) = create_work(manual_work, manual_work.mwActionIgOpt)
             ig_works_works.append(w)
+            if w_official_opt: ig_works_works_official.append(w_official_opt)
         if manual_work.mwActionTkOpt:
             (w,w_official_opt) = create_work(manual_work, manual_work.mwActionTkOpt)
             tk_works_works.append(w)
+            if w_official_opt: tk_works_works_official.append(w_official_opt)
     # Eu precisei criar o `SocialNetworkWorks` depois do `ig_works_works` pronto,
     # com os itens, porque ele não mantém o ponteiro, ele faz uma cópia dos itens
     # da lista
     ig_works:SocialNetworkWorks = SocialNetworkWorks(socialNetwork=SocialNetwork.SNInstagram, works=ig_works_works)
     tk_works:SocialNetworkWorks = SocialNetworkWorks(socialNetwork=SocialNetwork.SNTiktok, works=tk_works_works)
+
+    ig_works_official:SocialNetworkWorks = SocialNetworkWorks(socialNetwork=SocialNetwork.SNInstagram, works=ig_works_works_official)
+    tk_works_official:SocialNetworkWorks = SocialNetworkWorks(socialNetwork=SocialNetwork.SNTiktok, works=tk_works_works_official)
     
-    return [ig_works, tk_works]
+    return ([ig_works, tk_works], [ig_works_official, tk_works_official])
 
 def create_work(manual_work:ManualWork, manual_action:ManualAction) -> Tuple[Work, Optional[Work]]:
     msg_opt = manual_action.maMessageOpt
     smh_id = manual_action.maAccountOpt.maaSmhId if manual_action.maAccountOpt else manual_work.mwSmh.id
     w:Work = create_work2(smh_id, msg_opt, manual_work.mwFileCode)
-    smh_id_official_opt:Optional[Work] = manual_action.maAccountOfficialOpt.maaSmhId if manual_action.maAccountOfficialOpt else None
+    smh_id_official_opt:Optional[int] = manual_action.maAccountOfficialOpt.maaSmhId if manual_action.maAccountOfficialOpt else None
     w_official_opt:Work = create_work2(smh_id_official_opt, msg_opt, manual_work.mwFileCode) if smh_id_official_opt else None
     return (w,w_official_opt)
 
