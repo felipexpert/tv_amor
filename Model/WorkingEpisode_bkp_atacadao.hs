809f4a860c4135834fb6f19c1ef53dc3a078e2d9
Felipe Carmona Miquilini <felipe.miquilini@gmail.com>
2025-08-27 11:25:45 -0300

    progresso

--- Model/WorkingEpisode_bkp_atacadao.hs
+++ Model/WorkingEpisode_bkp_atacadao.hs
@@ -40,11 +40,11 @@ episodeSetup :: EpisodeSetup
 episodeSetup = EpisodeSetup
     { sSprites = 
         -- [ SSprite (EPeLabel "pe_felipe777") "02 sprite mega-sushi-temakeria IMPORT.psd" EPeNum1
-        [ SSprite (EPeLabel "pe_amanda") "cacau-show-itapira_amanda.psd" EPeNum1
+        [ SSprite (EPeLabel "pe_fabiana") "atacadao-dos-pisos.psd" EPeNum1
         , SSprite (EPeLabel "pe_mo") "melhores-ofertas.psd" EPeNum2
         ]
     , sBackgroundImage = SBackground
-        { bImagePath = "cacau-show-itapira-04.jpg"
+        { bImagePath = "atacadao-dos-pisos-03.jpg"
         , bWidth = 1080
         , bHeight = 1920
         , bSpritePositions = SPositionsFor2
@@ -56,46 +56,44 @@ episodeSetup = EpisodeSetup
 
 episode :: Episode
 episode = Episode
-    { ePes = [EPeLabel "pe_amanda", EPeLabel "pe_mo"]
-    , eDialoguePeList =
+    { ePes = [EPeLabel "pe_fabiana", EPeLabel "pe_mo"]
+    , eDialoguePeList = 
         [ EDialoguePe
             { dPe = EPeLabel "pe_mo"
-            , dContents =
-                [ RPlainText "Olá, Amanda da Cacau Show Itapira!"
+            , dContents = 
+                [ RPlainText "Olá, eu sou o Melhores Ofertas e vim conversar com a Fabiana, do Atacadão dos Pisos Itapira!"
                 , RCommand (CGesture GHi (EPeLabel "pe_mo"))
-                , RPlainText "Hoje vamos falar da tradição e qualidade da sua loja!"
+                , RCommand (CPause 500)
+                , RPlainText "Fabiana, qual o grande diferencial da sua loja?"
+                ]
+            }
+        , EDialoguePe
+            { dPe = EPeLabel "pe_fabiana"
+            , dContents =
+                [ RCommand (CGesture GStandShort (EPeLabel "pe_fabiana"))
+                , RPlainText "Com certeza, a nossa variedade! Temos pisos cerâmicos e porcelanatos de diversos tamanhos, cores e texturas. Do clássico ao moderno, tudo em um só lugar!"
+                , RCommand (CGesture GTalkLong (EPeLabel "pe_fabiana"))
+                ]
+            }
+        , EDialoguePe
+            { dPe = EPeLabel "pe_mo"
+            , dContents =
+                [ RPlainText "É muita opção pra quem quer reformar ou construir. E o melhor é que tem o preço do atacado!"
+                , RCommand (CGesture GExcited (EPeLabel "pe_mo"))
+                ]
+            }
+        , EDialoguePe
+            { dPe = EPeLabel "pe_fabiana"
+            , dContents =
+                [ RPlainText "Isso mesmo! Temos um mix completo para todos os estilos!"
+                , RCommand (CGesture GTalkShort (EPeLabel "pe_fabiana"))
+                ]
+            }
+        , EDialoguePe
+            { dPe = EPeLabel "pe_mo"
+            , dContents =
+                [ RPlainText "Já conhece o Atacadão dos Pisos? Conta pra gente nos comentários como foi sua experiência!"
                 , RCommand (CGesture GTalkShort (EPeLabel "pe_mo"))
-                ]
-            }
-        , EDialoguePe
-            { dPe = EPeLabel "pe_amanda"
-            , dContents =
-                [ RPlainText "Exato!"
-                , RCommand (CGesture GExcited (EPeLabel "pe_amanda"))
-                , RPlainText "Nossos chocolates são conhecidos por serem os melhores."
-                , RCommand (CGesture GTalkShort (EPeLabel "pe_amanda"))
-                ]
-            }
-        , EDialoguePe
-            { dPe = EPeLabel "pe_mo"
-            , dContents =
-                [ RPlainText "Uma qualidade que o cliente conhece e confia, né?"
-                , RCommand (CGesture GThinkShort (EPeLabel "pe_mo"))
-                ]
-            }
-        , EDialoguePe
-            { dPe = EPeLabel "pe_amanda"
-            , dContents =
-                [ RPlainText "Sim, e isso a gente entrega todos os dias!"
-                , RCommand (CGesture GTalkShort (EPeLabel "pe_amanda"))
-                ]
-            }
-        , EDialoguePe
-            { dPe = EPeLabel "pe_mo"
-            , dContents =
-                [ RPlainText "Já teve uma experiência bacana com a Cacau Show Itapira?"
-                , RCommand (CGesture GStandShort (EPeLabel "pe_mo"))
-                , RPlainText "Comente abaixo e inspire outros clientes a conhecerem também!"
                 ]
             }
         ]
